6726ba66c2e998758486c47c7cb8d68974a42b9c
Giovanni Gaffe <gaffejonathan@ymail.com>
2020-04-05 21:46:24 +0800

    We set webView method with escaping handler if we should allow loading

--- Project 4/ViewController.swift
+++ Project 4/ViewController.swift
@@ -12,41 +12,46 @@ import WebKit
 class ViewController: UIViewController, WKNavigationDelegate {
     var webView: WKWebView!
     var progressView: UIProgressView!
+    var websites = ["apple.com", "hackingwithswift.com"]
     
     override func loadView() {
         webView = WKWebView()
         webView.navigationDelegate = self
         view = webView
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-            
+        
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
         
         // .fexible space crate flexible place so no target and no action
         let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
         let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
-
+        
         progressView = UIProgressView(progressViewStyle: .default)
         progressView.sizeToFit()
         let progressButton = UIBarButtonItem(customView: progressView)
         
         toolbarItems = [progressButton, spacer, refresh]
         navigationController?.isToolbarHidden = false
-            
+        
         // addObserver have 4 parameters 1. who the observer is. 2. what properties we want to observe. 3. which value we want so newValue. 4. which context nil
         webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
-        let url = URL(string: "https://www.hackingwithswift.com")!
+        
+        let url = URL(string: "https://" + websites[0])!
         webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = false
+        
     }
     
     @objc
     func openTapped() {
         let ac = UIAlertController(title: "Open page...", message: nil, preferredStyle: .actionSheet)
-        ac.addAction(UIAlertAction(title: "apple.com", style: .default, handler: openPage))
-        ac.addAction(UIAlertAction(title: "hacking.swift.com", style: .default, handler: openPage))
+        
+        for website in websites {
+            ac.addAction(UIAlertAction(title: website, style: .default, handler: openPage))
+        }
         ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
         ac.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
         present(ac, animated: true)
@@ -68,6 +73,27 @@ class ViewController: UIViewController, WKNavigationDelegate {
         if keyPath == "estimatedProgress" {
             progressView.progress = Float(webView.estimatedProgress)
         }
+    }
+    
+    // method if page should be load or not
+    // escaping key word that so closure can escape the current method
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        // set url to the URL of navigation
+        let url = navigationAction.request.url
+        
+        // 1. Unwrapped value of url.host
+        // 2. Loop over websites array
+        // 3. check if each safe website exist in the host name
+        // 4. If all ok we allow loading
+        if let host = url?.host {
+            for website in websites {
+                if host.contains(website) {
+                    decisionHandler(.allow)
+                    return
+                }
+            }
+        }
+        decisionHandler(.cancel) // 5. We didn't found any response so cancel loading
     }
 }
 
