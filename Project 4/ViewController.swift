9559cee49c19f98b91b6cb86476ae15823a9b127
Giovanni Gaffe <gaffejonathan@ymail.com>
2020-04-05 17:07:45 +0800

    we modify viewDidLoad with 2 new UIBarButtonItem who have is own initializer then assign toolBar item to a new array containing this items

--- Project 4/ViewController.swift
+++ Project 4/ViewController.swift
@@ -22,9 +22,17 @@ class ViewController: UIViewController, WKNavigationDelegate {
         super.viewDidLoad()
             
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
+        
+        // .fexible space crate flexible place so no target and no action
+        let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
+
+        toolbarItems = [spacer, refresh]
+        navigationController?.isToolbarHidden = false
+        
         let url = URL(string: "https://www.hackingwithswift.com")!
         webView.load(URLRequest(url: url))
-        webView.allowsBackForwardNavigationGestures = true
+        webView.allowsBackForwardNavigationGestures = false
     }
     
     @objc
